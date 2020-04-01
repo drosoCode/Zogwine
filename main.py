e5b887ab80e04d72808089351fa4d8349adb62c6
Droso <internet-site@outlook.com>
2020-04-01 12:27:44 +0200

    Re-enabled tornado

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from log import logger
 from server import api, app
-"""
+
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
 from tornado.web import Application
-"""
+
 logger.info('Starting Program ...')
 
 #api.runScan()
-app.run(host='0.0.0.0', port=8080, threaded=True)
-"""
+#app.run(host='0.0.0.0', port=8080, threaded=True)
+
 tr = WSGIContainer(app)
 
 application = Application([
@@ -18,4 +18,3 @@ application = Application([
 
 application.listen(8080, address='0.0.0.0')
 IOLoop.instance().start()
-"""
