86f1320eb6a2b51c80736ce80303f4176894ad18
DrosoCode <internet-site@outlook.com>
2020-11-13 12:56:11 +0100

    Fix log crash

--- app/log.py
+++ app/log.py
@@ -14,7 +14,7 @@ def getLogger():
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
-    # logger.addHandler(file_handler)
+    logger.addHandler(file_handler)
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
     logger.addHandler(stream_handler)
@@ -26,6 +26,9 @@ def getLogs(lines=10):
         content = f.read().splitlines()
     count = len(content)
     data = []
+    if count - lines <= 0:
+        return content
+
     for i in range(count - lines, count):
         data.append(content[i])
     return data
