b1a7c38d44f87b18c612cebb25c89c88c3d34678
DrosoCode <internet-site@outlook.com>
2021-06-26 21:43:39 +0200

    Add provider configure base method

--- app/scrapers/BaseProvider.py
+++ app/scrapers/BaseProvider.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
 
 
-class BaseScraper(ABC):
-    @abstractmethod
-    def __init__(self, apikey=None):
-        self._apikey = apikey
+class BaseProvider(ABC):
+    def __init__(self):
+        self._scraperID = None
+        self._scraperData = None
+
+    def configure(self, scraperID, scraperData=None):
+        self._scraperID = scraperID
+        self._scraperData = scraperData
