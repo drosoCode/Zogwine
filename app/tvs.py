c6358f13cb886fb1fc5f1f117567dec768cd8046
DrosoCode <internet-site@outlook.com>
2021-11-13 23:49:49 +0100

    add season edition/deletion

--- app/tvs.py
+++ app/tvs.py
@@ -154,6 +154,79 @@ def delete_episode(idEpisode: int):
 
 # endregion
 
+# region SEASON
+
+# For GET methods, see SHOWS region
+
+########################################################## PUT 
+
+@tvs.route("<int:idShow>/season/<int:season>", methods=["PUT"])
+def tvs_editTVSSeasonData(idShow:int, season: int):
+    checkUser("admin")
+    allowedFields = [
+        "title",
+        "season",
+        "overview",
+        "icon",
+        "rating",
+        "premiered",
+        "scraperLink",
+        "forceUpdate",
+    ]
+    sqlConnection, cursor = getSqlConnection()
+    data = json.loads(request.data)
+    err = False
+    msg = ""
+
+    for i, val in data.items():
+        if i in allowedFields:
+            err, msg = tvs_checkPutField(i, val)
+            if err:
+                break
+            else:
+                val = msg
+
+            cursor.execute(
+                "UPDATE seasons SET " + i + " = %(val)s WHERE idShow = %(idShow)s AND season = %(season)s",
+                {"val": val, "idShow": idShow, "season": season},
+            )
+        else:
+            err = True
+            msg = "Unknown field"
+            break
+
+    if not err:
+        sqlConnection.commit()
+    sqlConnection.close()
+
+    if not err:
+        return jsonify({"status": "ok", "data": "ok"})
+    else:
+        return jsonify({"status": "err", "data": msg}), 400
+
+@tvs.route("<int:idShow>/season/<int:season>", methods=["DELETE"])
+def tvs_deleteTVSSeason(idShow:int, season: int):
+    checkUser("admin")
+
+    sqlConnection, cursor = getSqlConnection()
+    idS = {"id": idShow, "season": season}
+    cursor.execute(
+        "DELETE FROM status WHERE mediaType = 1 AND idMedia IN (SELECT idEpisode FROM episodes WHERE idShow = %(id)s AND season = %(season)s);",
+        idS,
+    )
+    cursor.execute(
+        "DELETE FROM video_files WHERE idVid IN (SELECT idVid FROM episodes WHERE idShow = %(id)s AND season = %(season)s);",
+        idS,
+    )
+    cursor.execute("DELETE FROM episodes WHERE idShow = %(id)s AND season = %(season)s", idS)
+    cursor.execute("DELETE FROM seasons WHERE idShow = %(id)s AND season = %(season)s", idS)
+    sqlConnection.commit()
+    sqlConnection.close()
+    return jsonify({"status": "ok", "data": "ok"})
+
+# endregion
+
+
 # region SHOW
 
 ################################# GET ####################################################
@@ -187,8 +260,8 @@ def get_season(idShow: int, season: int = None):
         dat["season"] = season
         s = "AND season = %(season)s "
     cursor.execute(
-        "SELECT title, overview, CONCAT('/api/core/image/',icon) AS icon,"
-        "season, premiered, "
+        "SELECT idShow, title, overview, CONCAT('/api/core/image/',icon) AS icon,"
+        "season, premiered, scraperLink, "
         "(SELECT COUNT(*) FROM episodes WHERE idShow = s.idShow AND season = s.season) AS episodes, "
         "(SELECT COUNT(watchCount) FROM status WHERE idMedia IN (SELECT idEpisode FROM episodes WHERE idShow = s.idShow AND season = s.season) AND mediaType = 1  AND watchCount > 0 AND idUser = %(idUser)s) AS watchedEpisodes "
         "FROM seasons s "
@@ -196,7 +269,10 @@ def get_season(idShow: int, season: int = None):
         "ORDER BY season;",
         dat,
     )
-    res = cursor.fetchall()
+    if season is None:
+        res = cursor.fetchall()
+    else:
+        res = cursor.fetchone()
     sqlConnection.close()
     return jsonify({"status": "ok", "data": res})
 
