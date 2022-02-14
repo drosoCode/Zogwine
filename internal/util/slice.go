8ab6ab28b9ad46f970169f7cc8a2ea9de843bb62
DrosoCode <internet-site@outlook.com>
2022-02-14 10:06:35 +0100

    add more generic type

--- internal/util/slice.go
+++ internal/util/slice.go
@@ -1,6 +1,6 @@
 package util
 
-func Contains[T int64 | float64 | string](slice []T, elem T) bool {
+func Contains[T comparable](slice []T, elem T) bool {
 	for _, x := range slice {
 		if x == elem {
 			return true
